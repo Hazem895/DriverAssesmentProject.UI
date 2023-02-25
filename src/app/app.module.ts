64f6eccbf1aad4b75f03d21b457b0024f2fcff32
HazimAttia <HAttia@path-solutions.com>
2023-02-25 04:26:59 +0200

    AddedLatestWork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DriversListComponent } from './Components/Drivers/drivers-list/drivers-list.component';
+import { AddDriversComponent } from './Components/Drivers/add-drivers/add-drivers.component';
+import { FormsModule } from '@angular/forms';
+import { EditDriverComponent } from './Components/Drivers/edit-driver/edit-driver.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DriversListComponent,
+    AddDriversComponent,
+    EditDriverComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
